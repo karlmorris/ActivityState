16c4748a3928fd2086a7766ec52f0d1dcb2dc1a6
Karl Morris <karl.morris@temple.edu>
2022-02-21 19:14:03 -0500

    Added onSaveInstanceState implementation

--- app/src/main/java/edu/temple/activitystate/MainActivity.kt
+++ app/src/main/java/edu/temple/activitystate/MainActivity.kt
@@ -8,11 +8,26 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    // A companion object is used here to store references
+    // Think of this as something similar to a static reference
+    // You can use 'const val', but constants have global namespace
+    companion object {
+        val KEY_PERSON = "personKey"
+    }
+
     var person: Person? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Fetch info from saveInstanceState, but only if it's not-null
+        // argument will be null first time activity loads, and will continue
+        // to be null if we do not store items in bundle argument of
+        // onSaveInstanceState()
+        savedInstanceState?.run {
+            person = Person(getString(KEY_PERSON)!!)
+        }
 
         val nameEditText = findViewById<EditText>(R.id.editTextTextPersonName)
 
@@ -24,7 +39,21 @@ class MainActivity : AppCompatActivity() {
             person?.run {
                 Toast.makeText(this@MainActivity, "Name: $name", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
 
+    // Alternative to onCreate()
+    // You can use one, but probably not both - Nothing will break, it's just inefficient
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        person = Person(savedInstanceState.getString(KEY_PERSON)!!)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // If person is not-null, then execute the scope function
+        person?.run{
+            outState.putString(KEY_PERSON, name)
         }
     }
 }
